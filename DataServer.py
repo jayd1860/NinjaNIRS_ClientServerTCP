bf12d7b41b355d5ee829dc308dab24268de3005a
jayd1860 <jdubb@bu.edu>
2023-06-05 16:07:18 -0400

    -- Convert displayed IP address from bytes to string

--- DataServer.py
+++ DataServer.py
@@ -13,12 +13,12 @@ def SendData():
     #####################################################################
     server_address0 = ('', Settings.port0)
     s0.bind(server_address0)
-    sys.stdout.write('DataServer: Waiting to receive IP from client ...\n')
+    sys.stdout.write('DataServer:  Waiting to receive IP from client ...\n')
     while True:
         # Receive the client packet along with the address it is coming from
         serverIpAddr, clientIpAddr = s0.recvfrom(128)
         if len(serverIpAddr) > 0:
-            sys.stdout.write('DataServer:  Received own IP address %s\n'% serverIpAddr)
+            sys.stdout.write('DataServer:  Received own IP address %s\n'% serverIpAddr.decode())
             time.sleep(2)
             s0.sendto(b'DataServer: We received our IP address', (clientIpAddr[0], Settings.port0))
             break
@@ -32,7 +32,7 @@ def SendData():
     server_address = (serverIpAddr, Settings.port)
 
     # Bind the socket to server (our own local) address and local port
-    sys.stdout.write('DataServer:  Binding socket to server IP address %s on port %d\n'% (serverIpAddr, Settings.port))
+    sys.stdout.write('DataServer:  Binding socket to server IP address %s on port %d\n'% (serverIpAddr.decode(), Settings.port))
     s.bind(server_address)
     s.listen(1)
     sys.stdout.write("DataServer:  Waiting for connection  ...\n")
